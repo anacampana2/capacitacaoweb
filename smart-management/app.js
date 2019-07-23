5ff363da1b222c0bf27813f2b21d78226aa8037c
Ana Clara Campana <anacampana@cpejr.com.br>
2019-07-23 11:54:12 -0300

    cadastro

--- smart-management/app.js
+++ smart-management/app.js
@@ -1,22 +1,38 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var sassMiddleware = require('node-sass-middleware');
+require('dotenv').config();
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const sassMiddleware = require('node-sass-middleware');
+const firebase = require('firebase');
+const bodyParser = require('body-parser');
+const session = require('express-session');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
 var app = express();
+
+ /* firebase setup
+ */
+const config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+};
+firebase.initializeApp(config);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(sassMiddleware({
   src: path.join(__dirname, 'public'),
--- smart-management/routes/index.js
+++ smart-management/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const firebase = require('firebase');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,5 +9,29 @@ router.get('/', function(req, res, next) {
 router.get('/login', function(req, res, next) {
   res.render('login', { title: 'Login' });
 });
-
+router.get('/cadastro', function(req, res, next) {
+  res.render('cadastro', { title: 'Cadastro' });
+});
+router.post('/cadastro', function(req, res, next) {
+  const user = req.body.user;
+  console.log("deu certo");
+  console.log(user);
+  firebase.auth().createUserWithEmailAndPassword(user.email,user.password).then((userF)=>{
+    res.redirect('/login');
+    console.log(userF);
+  }).catch((error)=>{
+    res.redirect('/login');
+    console.log("error");
+  });
+});
+router.post('/login', function(req, res, next) {
+  const user = req.body.user;
+  console.log(user);
+  firebase.auth().signInWithEmailAndPassword(user.email,user.password).then((logado)=>{
+    console.log(logado);
+    res.redirect('/login');
+  }).catch((error)=>{
+    console.log(error);
+  });
+});
 module.exports = router;
