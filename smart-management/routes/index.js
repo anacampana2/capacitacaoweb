b0fd30e70b3bafd98c0679ba9a357c1d8262b2a4
Gabriel Alves <gabrielalves98@hotmail.com>
2019-07-23 19:29:52 -0300

    Cadastro

--- smart-management/routes/index.js
+++ smart-management/routes/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const firebase = require('firebase');
 const router = express.Router();
 
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -18,6 +17,20 @@ router.post('/login', function(req, res, next) {
     res.redirect('/login');
   }).catch((error) => {
     console.log(error);
+  });
+});
+router.get('/cadastro', function(req, res, next) {
+  res.render('cadastro', { title: 'Cadastro' });
+});
+router.post('/cadastro', function(req, res, next) {
+  const user = req.body.user;
+  console.log(user);
+  firebase.auth().createUserWithEmailAndPassword(user.email,user.password).then((userF)=>{
+    res.redirect('/login');
+    console.log(userF);
+  }).catch((error)=>{
+    res.redirect('/login');
+    console.log("error");
   });
 });
 
