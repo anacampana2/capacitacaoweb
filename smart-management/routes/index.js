c4c788b9d61a7ad191078f78f2ce2cf1619eba73
Gabriel Alves <gabrielalves98@hotmail.com>
2019-07-23 11:12:50 -0300

    Aula 2

--- smart-management/routes/index.js
+++ smart-management/routes/index.js
@@ -1,5 +1,7 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const firebase = require('firebase');
+const router = express.Router();
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,6 +9,16 @@ router.get('/', function(req, res, next) {
 });
 router.get('/login', function(req, res, next) {
   res.render('login', { title: 'Login' });
+});
+router.post('/login', function(req, res, next) {
+  const user = req.body.user;
+  console.log(user);
+  firebase.auth().signInWithEmailAndPassword(user.email, user.password).then((logado) => {
+    console.log(logado);
+    res.redirect('/login');
+  }).catch((error) => {
+    console.log(error);
+  });
 });
 
 module.exports = router;
